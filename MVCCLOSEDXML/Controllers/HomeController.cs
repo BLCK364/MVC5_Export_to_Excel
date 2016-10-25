de4e42d7a0a42635481472b5616724b2cf716d76
Fernando Bachur <fbachurm@gmail.com>
2016-10-25 10:26:52 -0300

    Master, proyecto base | base project

---
+++ MVCCLOSEDXML/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.IO;
+using ClosedXML.Excel;
+
+namespace MVCCLOSEDXML.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public void Excel()
+        {
+            /*http://closedxml.codeplex.com/documentation
+             * Documentación oficial de la libreria para poder manipular el excel como gustes.
+             * */
+            DataTable dt = new DataTable("GridView_Data");
+            //Columnas
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("NOMBRE", typeof(string));
+            dt.Columns.Add("EMAIL", typeof(string));
+            dt.Columns.Add("ESTADO", typeof(string));
+            //Datos
+            dt.Rows.Add(1, "FERNANDO BACHUR", "XXXXXXXXX@GMAIL.COM", "ACTIVO");
+            dt.Rows.Add(2, "MICHAEL LOBOS", "XXXXXXXXX@GMAIL.COM", "ACTIVO");
+            dt.Rows.Add(3, "ALEJANDRO COMAS", "XXXXXXXXX@GMAIL.COM", "ACTIVO");
+            dt.Rows.Add(4, "ROBERTO LAZO", "XXXXXXXXX@GMAIL.COM", "ACTIVO");
+            dt.Rows.Add(5, "VICTOR RAMOS", "XXXXXXXXX@GMAIL.COM", "ACTIVO");
+            //Creacion y entrega FBM
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                wb.Worksheets.Add(dt);
+
+                Response.Clear();
+                Response.Buffer = true;
+                Response.Charset = "";
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", "attachment;filename=Listado.xlsx");
+                using (MemoryStream MyMemoryStream = new MemoryStream())
+                {
+                    wb.SaveAs(MyMemoryStream);
+                    MyMemoryStream.WriteTo(Response.OutputStream);
+                    Response.Flush();
+                    Response.End();
+                }
+            }
+        }
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # MVC5_Export_to_Excel
 Export data to Excel file with ClosedXML Library
+
+Esta es la primera vez que ocupo Github, quiero subir esto ya que revise todos los repositorios que se relacionaban con
+proyectos "MVC Export Data to Excel" y todos los que baje notificaba la alerta de que el archivo generado podía ser dañino(Inseguro).
+
+Si pueden colaborar bienvenido sea, esto es solo un ejemplo básico para integrar la función exportar en pocas líneas.
+Saludos,
+
+Atte.
+Fernando Bachur M.
+fbachurm@gmail.com
+Whatsapp: +56997462985
+Santiago, Chile.
+
